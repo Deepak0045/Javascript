68d5db9da62924ff3064fd1f12159a530f065299
Deepak Yadav <deepakry937@gmail.com>
2024-11-07 16:08:28 +0000

    type conversion Basics

---
+++ 01_Basics/03_TypeConveresion.js
@@ -0,0 +1,19 @@
+let score = 33
+console.log(typeof score);
+
+let score2 = '33ab'
+console.log(typeof score2);
+
+let valueInNumber = Number(score2)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// '33' ==> 33
+// '33abc' ==> NaN
+// true ==> 1
+
+let someNumber = 33
+
+let stringnumber = String(someNumber)
+console.log(stringnumber); //33
+console.log(typeof(stringnumber));   // string
