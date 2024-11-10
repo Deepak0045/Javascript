fb80400e4e92e957f250eeb437d98d6d4a1cbe62
Deepak Yadav <deepakry937@gmail.com>
2024-11-10 21:34:55 +0530

    Update 02_Datatypes.js

--- 01_Basics/02_Datatypes.js
+++ 01_Basics/02_Datatypes.js
@@ -1,7 +1,12 @@
 'use strict'; // treat all code as newer version
 
+// Primitive Datatypes
+
+// 7 Types : String, Number, Boolean, Null, undefined, Symbol, BigInt
+
 
 let state = null
+
 // String
 let greeting = "Hello, World!";
 console.log("String:", greeting); // Output: String: Hello, World!
@@ -22,14 +27,6 @@ console.log("Null:", emptyValue); // Output: Null: null
 let unassignedVar;
 console.log("Undefined:", unassignedVar); // Output: Undefined: undefined
 
-// Object
-let person = { name: "Alice", age: 30 };
-console.log("Object:", person); // Output: Object: { name: "Alice", age: 30 }
-
-// Array
-let colors = ["red", "green", "blue"];
-console.log("Array:", colors); // Output: Array: ["red", "green", "blue"]
-
 // Symbol
 let uniqueId = Symbol("id");
 console.log("Symbol:", uniqueId); // Output: Symbol: Symbol(id)
@@ -39,6 +36,26 @@ let largeNumber = 123456789012345678901234567890n;
 console.log("BigInt:", largeNumber); // Output: BigInt: 123456789012345678901234567890n
 
 
-console.log('Deepak');
+// Reference or Non-Primitive
 
-console.log(typeof state);
+// Array, objects, Functions
+
+
+// Array
+let colors = ["red", "green", "blue"];
+console.log("Array:", colors); // Output: Array: ["red", "green", "blue"]
+
+
+// Object
+let person = { name: "Alice", age: 30 };
+console.log("Object:", person); // Output: Object: { name: "Alice", age: 30 }
+
+
+// Function can be treated as varaiable
+
+const myFunction = function(){
+    console.log('Hello World');
+    
+}
+
+console.log(typeof myFunction);
